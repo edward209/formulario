8b4de98e42735d33f1b1cca4133d22b581f9543b
edward209 <franciscoedward296@gmail.com>
2025-06-28 15:11:43 -0400

    Rango de Primos

---
+++ RangoPrimosApp/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace RangoPrimosApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInicio = new System.Windows.Forms.Label();
+            this.txtInicio = new System.Windows.Forms.TextBox();
+            this.lblFin = new System.Windows.Forms.Label();
+            this.txtFin = new System.Windows.Forms.TextBox();
+            this.btnBuscarPrimos = new System.Windows.Forms.Button();
+            this.lstPrimos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblInicio
+            // 
+            this.lblInicio.AutoSize = true;
+            this.lblInicio.Location = new System.Drawing.Point(12, 21);
+            this.lblInicio.Name = "lblInicio";
+            this.lblInicio.Size = new System.Drawing.Size(82, 13);
+            this.lblInicio.TabIndex = 0;
+            this.lblInicio.Text = "Inicio del rango:";
+            // 
+            // txtInicio
+            // 
+            this.txtInicio.Location = new System.Drawing.Point(101, 21);
+            this.txtInicio.Name = "txtInicio";
+            this.txtInicio.Size = new System.Drawing.Size(100, 20);
+            this.txtInicio.TabIndex = 1;
+            this.txtInicio.Text = "1";
+            // 
+            // lblFin
+            // 
+            this.lblFin.AutoSize = true;
+            this.lblFin.Location = new System.Drawing.Point(15, 59);
+            this.lblFin.Name = "lblFin";
+            this.lblFin.Size = new System.Drawing.Size(71, 13);
+            this.lblFin.TabIndex = 2;
+            this.lblFin.Text = "Fin del rango:";
+            // 
+            // txtFin
+            // 
+            this.txtFin.Location = new System.Drawing.Point(101, 51);
+            this.txtFin.Name = "txtFin";
+            this.txtFin.Size = new System.Drawing.Size(100, 20);
+            this.txtFin.TabIndex = 3;
+            this.txtFin.Text = "100";
+            // 
+            // btnBuscarPrimos
+            // 
+            this.btnBuscarPrimos.Location = new System.Drawing.Point(101, 77);
+            this.btnBuscarPrimos.Name = "btnBuscarPrimos";
+            this.btnBuscarPrimos.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscarPrimos.TabIndex = 4;
+            this.btnBuscarPrimos.Text = "Buscar Números Primos";
+            this.btnBuscarPrimos.UseVisualStyleBackColor = true;
+            this.btnBuscarPrimos.Click += new System.EventHandler(this.btnBuscarPrimos_Click);
+            // 
+            // lstPrimos
+            // 
+            this.lstPrimos.FormattingEnabled = true;
+            this.lstPrimos.Location = new System.Drawing.Point(101, 106);
+            this.lstPrimos.Name = "lstPrimos";
+            this.lstPrimos.ScrollAlwaysVisible = true;
+            this.lstPrimos.Size = new System.Drawing.Size(122, 121);
+            this.lstPrimos.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 237);
+            this.Controls.Add(this.lstPrimos);
+            this.Controls.Add(this.btnBuscarPrimos);
+            this.Controls.Add(this.txtFin);
+            this.Controls.Add(this.lblFin);
+            this.Controls.Add(this.txtInicio);
+            this.Controls.Add(this.lblInicio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInicio;
+        private System.Windows.Forms.TextBox txtInicio;
+        private System.Windows.Forms.Label lblFin;
+        private System.Windows.Forms.TextBox txtFin;
+        private System.Windows.Forms.Button btnBuscarPrimos;
+        private System.Windows.Forms.ListBox lstPrimos;
+    }
+}
+
---
+++ RangoPrimosApp/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RangoPrimosApp 
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void btnBuscarPrimos_Click(object sender, EventArgs e)
+        {
+   
+            lstPrimos.Items.Clear();
+
+
+            string textoInicio = txtInicio.Text.Trim();
+            string textoFin = txtFin.Text.Trim();
+
+            int inicioRango;
+            int finRango;
+
+
+            if (!int.TryParse(textoInicio, out inicioRango) || inicioRango < 0)
+            {
+                MessageBox.Show("Por favor, ingresa un número válido y no negativo para el inicio del rango.", "Error de Entrada", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtInicio.Focus();
+                return;
+            }
+
+ 
+            if (!int.TryParse(textoFin, out finRango) || finRango < 0)
+            {
+                MessageBox.Show("Por favor, ingresa un número válido y no negativo para el fin del rango.", "Error de Entrada", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtFin.Focus();
+                return;
+            }
+
+
+            if (inicioRango > finRango)
+            {
+                MessageBox.Show("El valor de inicio del rango no puede ser mayor que el valor de fin.", "Error de Rango", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtInicio.Focus();
+                return;
+            }
+
+
+            for (int i = inicioRango; i <= finRango; i++)
+            {
+                if (EsNumeroPrimo(i))
+                {
+                    lstPrimos.Items.Add(i); 
+                }
+            }
+
+
+            if (lstPrimos.Items.Count == 0)
+            {
+                MessageBox.Show($"No se encontraron números primos en el rango de {inicioRango} a {finRango}.", "Sin Primos", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+        /// <param name="num"
+
+        private bool EsNumeroPrimo(int num)
+        {
+            if (num <= 1)
+                return false;
+
+
+            if (num == 2)
+                return true;
+
+
+            if (num % 2 == 0)
+                return false;
+
+
+            for (int i = 3; i * i <= num; i += 2)
+            {
+                if (num % i == 0)
+                    return false; 
+            }
+
+            return true; 
+        }
+
+
+        private void txtInicio_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstPrimos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
