547b5d8eda8cb1dab29d2c699076bcd59c3d6e2b
edward209 <franciscoedward296@gmail.com>
2025-06-28 15:10:01 -0400

    Pilandromo

---
+++ PalindromoApp/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PalindromoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstruccion = new System.Windows.Forms.Label();
+            this.txtPalabra = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInstruccion
+            // 
+            this.lblInstruccion.AutoSize = true;
+            this.lblInstruccion.Location = new System.Drawing.Point(12, 23);
+            this.lblInstruccion.Name = "lblInstruccion";
+            this.lblInstruccion.Size = new System.Drawing.Size(104, 13);
+            this.lblInstruccion.TabIndex = 0;
+            this.lblInstruccion.Text = "Ingresa una palabra:";
+            // 
+            // txtPalabra
+            // 
+            this.txtPalabra.Location = new System.Drawing.Point(123, 23);
+            this.txtPalabra.Name = "txtPalabra";
+            this.txtPalabra.Size = new System.Drawing.Size(100, 20);
+            this.txtPalabra.TabIndex = 1;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(123, 59);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.TabIndex = 2;
+            this.btnVerificar.Text = "Verificar Palíndromo";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 170);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txtPalabra);
+            this.Controls.Add(this.lblInstruccion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstruccion;
+        private System.Windows.Forms.TextBox txtPalabra;
+        private System.Windows.Forms.Button btnVerificar;
+    }
+}
+
---
+++ PalindromoApp/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq; 
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PalindromoApp 
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+           
+            string palabraOriginal = txtPalabra.Text;
+
+           
+            if (string.IsNullOrWhiteSpace(palabraOriginal))
+            {
+                MessageBox.Show("Por favor, ingresa una palabra antes de verificar.", "Entrada Vacía", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtPalabra.Focus(); 
+                return; 
+            }
+
+           
+            string palabraLimpia = palabraOriginal.ToLower().Replace(" ", "");
+
+           
+            char[] arrayCaracteres = palabraLimpia.ToCharArray();
+            Array.Reverse(arrayCaracteres);
+            string palabraInvertida = new string(arrayCaracteres);
+
+
+            if (palabraLimpia == palabraInvertida)
+            {
+                MessageBox.Show($"'{palabraOriginal}' ES un palíndromo.", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show($"'{palabraOriginal}' NO es un palíndromo.", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            txtPalabra.Clear(); 
+            txtPalabra.Focus(); 
+        }
+
+       
+        private void txtPalabra_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
