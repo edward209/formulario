e0f8dda6fe193a7fc5b0dfcc308e8c9ca359cc5b
edward209 <franciscoedward296@gmail.com>
2025-06-28 15:13:13 -0400

    Temporizador

---
+++ TemporizadorApp/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace TemporizadorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblMinutos = new System.Windows.Forms.Label();
+            this.txtMinutos = new System.Windows.Forms.TextBox();
+            this.lblSegundos = new System.Windows.Forms.Label();
+            this.txtSegundos = new System.Windows.Forms.TextBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.lblTiempoRestante = new System.Windows.Forms.Label();
+            this.cronometro = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblMinutos
+            // 
+            this.lblMinutos.AutoSize = true;
+            this.lblMinutos.Location = new System.Drawing.Point(12, 35);
+            this.lblMinutos.Name = "lblMinutos";
+            this.lblMinutos.Size = new System.Drawing.Size(47, 13);
+            this.lblMinutos.TabIndex = 0;
+            this.lblMinutos.Text = "Minutos:";
+            // 
+            // txtMinutos
+            // 
+            this.txtMinutos.Location = new System.Drawing.Point(95, 35);
+            this.txtMinutos.Name = "txtMinutos";
+            this.txtMinutos.Size = new System.Drawing.Size(100, 20);
+            this.txtMinutos.TabIndex = 1;
+            this.txtMinutos.Text = "0";
+            this.txtMinutos.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtMinutos.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblSegundos
+            // 
+            this.lblSegundos.AutoSize = true;
+            this.lblSegundos.Location = new System.Drawing.Point(12, 68);
+            this.lblSegundos.Name = "lblSegundos";
+            this.lblSegundos.Size = new System.Drawing.Size(58, 13);
+            this.lblSegundos.TabIndex = 2;
+            this.lblSegundos.Text = "Segundos:";
+            // 
+            // txtSegundos
+            // 
+            this.txtSegundos.Location = new System.Drawing.Point(95, 68);
+            this.txtSegundos.Name = "txtSegundos";
+            this.txtSegundos.Size = new System.Drawing.Size(100, 20);
+            this.txtSegundos.TabIndex = 3;
+            this.txtSegundos.Text = "0";
+            this.txtSegundos.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(218, 47);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 4;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Enabled = false;
+            this.btnDetener.Location = new System.Drawing.Point(308, 47);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(75, 23);
+            this.btnDetener.TabIndex = 5;
+            this.btnDetener.Text = "Reiniciar";
+            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // lblTiempoRestante
+            // 
+            this.lblTiempoRestante.AutoSize = true;
+            this.lblTiempoRestante.Font = new System.Drawing.Font("Stencil", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTiempoRestante.Location = new System.Drawing.Point(231, 85);
+            this.lblTiempoRestante.Name = "lblTiempoRestante";
+            this.lblTiempoRestante.Size = new System.Drawing.Size(152, 57);
+            this.lblTiempoRestante.TabIndex = 6;
+            this.lblTiempoRestante.Text = "00:00";
+            this.lblTiempoRestante.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // cronometro
+            // 
+            this.cronometro.Interval = 1000;
+            this.cronometro.Tick += new System.EventHandler(this.cronometro_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 213);
+            this.Controls.Add(this.lblTiempoRestante);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.txtSegundos);
+            this.Controls.Add(this.lblSegundos);
+            this.Controls.Add(this.txtMinutos);
+            this.Controls.Add(this.lblMinutos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMinutos;
+        private System.Windows.Forms.TextBox txtMinutos;
+        private System.Windows.Forms.Label lblSegundos;
+        private System.Windows.Forms.TextBox txtSegundos;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.Label lblTiempoRestante;
+        private System.Windows.Forms.Timer cronometro;
+    }
+}
+
---
+++ TemporizadorApp/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemporizadorApp 
+{
+    public partial class Form1 : Form
+    {
+        private int segundosTotales;    
+        private int segundosRestantes;  
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+
+
+            lblTiempoRestante.Text = "00:00";
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+
+            int minutosInput;
+            int segundosInput;
+
+
+            if (!int.TryParse(txtMinutos.Text, out minutosInput) || minutosInput < 0)
+            {
+                MessageBox.Show("Por favor, ingresa un valor válido para minutos (número entero no negativo).", "Error de Entrada", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtMinutos.Focus(); 
+                return; 
+            }
+
+
+            if (!int.TryParse(txtSegundos.Text, out segundosInput) || segundosInput < 0 || segundosInput >= 60)
+            {
+                MessageBox.Show("Por favor, ingresa un valor válido para segundos (número entero entre 0 y 59).", "Error de Entrada", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtSegundos.Focus(); 
+                return; 
+            }
+
+
+            segundosTotales = (minutosInput * 60) + segundosInput;
+
+
+            if (segundosTotales <= 0)
+            {
+                MessageBox.Show("El tiempo total configurado debe ser mayor a cero.", "Error de Entrada", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtMinutos.Focus(); 
+                return; 
+            }
+
+
+            segundosRestantes = segundosTotales;
+
+
+            ActualizarTiempoRestanteUI();
+
+
+            txtMinutos.Enabled = false; 
+            txtSegundos.Enabled = false; 
+            btnIniciar.Enabled = false;  
+            btnDetener.Enabled = true;   
+
+
+            cronometro.Start();
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            cronometro.Stop(); 
+
+            
+            txtMinutos.Enabled = true;   
+            txtSegundos.Enabled = true;  
+            btnIniciar.Enabled = true;   
+            btnDetener.Enabled = false;  
+
+            lblTiempoRestante.Text = "00:00"; 
+
+            
+            
+            
+        }
+
+        
+        private void cronometro_Tick(object sender, EventArgs e)
+        {
+            segundosRestantes--; 
+
+
+            ActualizarTiempoRestanteUI();
+
+         
+            if (segundosRestantes <= 0)
+            {
+                cronometro.Stop(); 
+
+                
+                txtMinutos.Enabled = true;
+                txtSegundos.Enabled = true;
+                btnIniciar.Enabled = true;
+                btnDetener.Enabled = false;
+                lblTiempoRestante.Text = "00:00"; 
+
+         
+                MessageBox.Show("¡Tiempo agotado!", "Temporizador", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+        private void ActualizarTiempoRestanteUI()
+        {
+
+            int minutos = segundosRestantes / 60;
+
+            int segundos = segundosRestantes % 60;
+
+
+            lblTiempoRestante.Text = $"{minutos:D2}:{segundos:D2}";
+        }
+
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
