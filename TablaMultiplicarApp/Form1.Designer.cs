43fced2380d5deccd4207f524c2cfb73e4861ab9
edward209 <franciscoedward296@gmail.com>
2025-06-28 15:12:28 -0400

    Tabla de Multiplicación

---
+++ TablaMultiplicarApp/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TablaMultiplicarApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstruccion = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.lstTabla = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblInstruccion
+            // 
+            this.lblInstruccion.AutoSize = true;
+            this.lblInstruccion.Location = new System.Drawing.Point(12, 43);
+            this.lblInstruccion.Name = "lblInstruccion";
+            this.lblInstruccion.Size = new System.Drawing.Size(98, 13);
+            this.lblInstruccion.TabIndex = 0;
+            this.lblInstruccion.Text = "Ingresa un número:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(116, 43);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(116, 69);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerar.TabIndex = 2;
+            this.btnGenerar.Text = "Generar Tabla";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // lstTabla
+            // 
+            this.lstTabla.FormattingEnabled = true;
+            this.lstTabla.Location = new System.Drawing.Point(116, 108);
+            this.lstTabla.Name = "lstTabla";
+            this.lstTabla.Size = new System.Drawing.Size(75, 134);
+            this.lstTabla.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 251);
+            this.Controls.Add(this.lstTabla);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblInstruccion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstruccion;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.ListBox lstTabla;
+    }
+}
+
---
+++ TablaMultiplicarApp/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TablaMultiplicarApp 
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+        
+            lstTabla.Items.Clear();
+
+           
+            string textoNumero = txtNumero.Text;
+
+           
+            int numero;
+            bool esNumeroValido = int.TryParse(textoNumero, out numero);
+
+            if (!esNumeroValido)
+            {
+                MessageBox.Show("Por favor, ingresa un número entero válido.", "Error de Entrada", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtNumero.Focus(); 
+                return; 
+            }
+
+            
+            if (numero < 1 || numero > 100) 
+            {
+                MessageBox.Show("Por favor, ingresa un número entre 1 y 100.", "Número Fuera de Rango", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtNumero.Focus();
+                return;
+            }
+
+            
+            for (int i = 1; i <= 10; i++) 
+            {
+                int resultado = numero * i;
+
+                lstTabla.Items.Add($"{numero} x {i} = {resultado}");
+            }
+        }
+
+        
+        private void txtNumero_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstTabla_SelectedIndexChanged(object sender, EventArgs e)
+        {
+      
+        }
+    }
+}
