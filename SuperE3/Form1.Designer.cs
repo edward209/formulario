1a4cb8a87570cc2856fd1e7cea5f455a78b11351
edward209 <franciscoedward296@gmail.com>
2025-06-28 15:15:13 -0400

    SuperE3

---
+++ SuperE3/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SuperE3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(118, 12);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(16, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(313, 181);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 237);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ SuperE3/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperE3
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<char, int[,]> mapaNumeros;
+        private string numeroActual = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InicializarMapaNumeros();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            numeroActual = textBox1.Text;
+            panel1.Invalidate(); 
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            int size = 20;
+            int espacio = 10;
+            int xInicio = 10;
+
+            foreach (char c in numeroActual)
+            {
+                if (mapaNumeros.ContainsKey(c))
+                {
+                    var matriz = mapaNumeros[c];
+                    for (int fila = 0; fila < matriz.GetLength(0); fila++)
+                    {
+                        for (int col = 0; col < matriz.GetLength(1); col++)
+                        {
+                            if (matriz[fila, col] == 1)
+                            {
+                                e.Graphics.FillRectangle(
+                                    Brushes.Red,
+                                    xInicio + col * size,
+                                    fila * size,
+                                    size - 2,
+                                    size - 2);
+                            }
+                        }
+                    }
+                    xInicio += (matriz.GetLength(1) * size) + espacio;
+                }
+            }
+        }
+
+        private void InicializarMapaNumeros()
+        {
+            mapaNumeros = new Dictionary<char, int[,]>
+            {
+                { '0', new int[,] {
+                    {1,1,1},
+                    {1,0,1},
+                    {1,0,1},
+                    {1,0,1},
+                    {1,1,1} } },
+
+                { '1', new int[,] {
+                    {0,1,0},
+                    {1,1,0},
+                    {0,1,0},
+                    {0,1,0},
+                    {1,1,1} } },
+
+                { '2', new int[,] {
+                    {1,1,1},
+                    {0,0,1},
+                    {1,1,1},
+                    {1,0,0},
+                    {1,1,1} } },
+
+                { '3', new int[,] {
+                    {1,1,1},
+                    {0,0,1},
+                    {0,1,1},
+                    {0,0,1},
+                    {1,1,1} } },
+
+                { '4', new int[,] {
+                    {1,0,1},
+                    {1,0,1},
+                    {1,1,1},
+                    {0,0,1},
+                    {0,0,1} } },
+
+                { '5', new int[,] {
+                    {1,1,1},
+                    {1,0,0},
+                    {1,1,1},
+                    {0,0,1},
+                    {1,1,1} } },
+
+                { '6', new int[,] {
+                    {1,1,1},
+                    {1,0,0},
+                    {1,1,1},
+                    {1,0,1},
+                    {1,1,1} } },
+
+                { '7', new int[,] {
+                    {1,1,1},
+                    {0,0,1},
+                    {0,1,0},
+                    {0,1,0},
+                    {0,1,0} } },
+
+                { '8', new int[,] {
+                    {1,1,1},
+                    {1,0,1},
+                    {1,1,1},
+                    {1,0,1},
+                    {1,1,1} } },
+
+                { '9', new int[,] {
+                    {1,1,1},
+                    {1,0,1},
+                    {1,1,1},
+                    {0,0,1},
+                    {1,1,1} } }
+            };
+        }
+    }
+}
+
